fb33d5d1b1d0a5e4c9a47ae07656f8db34eb6e11
ehacinom <ehacinom@gmail.com>
2017-05-22 15:35:35 -0500

    albums and single album view refactored

--- browser/react/containers/AlbumsContainer.js
+++ browser/react/containers/AlbumsContainer.js
@@ -1,28 +1,12 @@
-import React, {Component} from 'react';
-import store from '../store';
+import {connect} from 'react-redux';
 import Albums from '../components/Albums';
 
-class AlbumsContainer extends Component {
-
-  constructor() {
-    super();
-    this.state = store.getState().albums;
-  }
-
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState().albums);
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    return <Albums albums={this.state.list}/>;
-  }
-
+const mapStateToProps = function (state) {
+    return {
+        albums: state.albums.list
+    }
 }
+const mapDispatchToProps = dispatch => ({});
 
+const AlbumsContainer = connect(mapStateToProps, mapDispatchToProps)(Albums);
 export default AlbumsContainer;
